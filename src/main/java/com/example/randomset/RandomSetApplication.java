c342b0a0c796bc6365ed70624115faaccc7fd4a0
yunfan <376550362@qq.com>
2022-08-30 00:32:28 +0800

    首次创建springboot空项目

---
+++ src/main/java/com/example/randomset/RandomSetApplication.java
@@ -0,0 +1,13 @@
+package com.example.randomset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/randomset/RandomSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
