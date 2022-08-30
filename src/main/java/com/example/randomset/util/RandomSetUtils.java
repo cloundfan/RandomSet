28b63d61bb2ff48106a23672bec4f8b7809e4480
yunfan <376550362@qq.com>
2022-08-30 13:10:52 +0800

    新增不同测试数据的测试类

--- src/main/java/com/example/randomset/util/RandomSetUtils.java
+++ src/main/java/com/example/randomset/util/RandomSetUtils.java
@@ -1,6 +1,7 @@
 package com.example.randomset.util;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -18,7 +19,7 @@ public class RandomSetUtils {
      * @param m 生成不重复随机数个数
      * @return {@link Set}<{@link Integer}>
      */
-    public static Set<Integer> random1(int n, int m){
+    public static Set<Integer> randomSet1(int n, int m){
         Set<Integer> set = new HashSet<Integer>();
         while(set.size()<m){
             set.add(ThreadLocalRandom.current().nextInt(n) + 1);
@@ -34,7 +35,7 @@ public class RandomSetUtils {
      * @param m 生成不重复随机数个数
      * @return {@link int[]}
      */
-    public static int[] random2(int n, int m){
+    public static int[] randomSet2(int n, int m){
         byte[] arrn = new byte[n];
         int[] arrm = new int[m];
         int j = 0;
@@ -56,7 +57,7 @@ public class RandomSetUtils {
      * @param m 生成不重复随机数个数
      * @return {@link int[]}
      */
-    public static int[] random3(int n, int m){
+    public static int[] randomSet3(int n, int m){
         int[] arrm = new int[m];
         int k = 0;
         int j = 0;
@@ -70,5 +71,37 @@ public class RandomSetUtils {
             j++;
         }
         return arrm;
+    }
+
+
+    /**
+     * 使用交换数组位置的方法生成不重复随机数实现4
+     *
+     * @param n 随机数取值范围
+     * @param m 生成不重复随机数个数
+     * @return {@link int[]}
+     */
+    public static int[] randomSet4(int n, int m) {
+        int[] readBall = new int[n];//红球号码池
+        int[] userReadBall = new int[m];//用户选择的红球号码
+        //为红球号码池赋值
+        Random r= new Random();
+        int index = -1;
+        for(int i=0;i<userReadBall.length;i++) {
+            index = r.nextInt(readBall.length-i);
+            if(readBall[index] == 0){
+                userReadBall[i] = index + 1;
+//                int temp = index + 1;
+                readBall[index] = readBall.length-i;
+//                readBall[readBall.length-i-1] = index + 1;
+            } else {
+                userReadBall[i] = readBall[index];
+//                int temp = readBall[index];
+                readBall[index] = readBall.length-i;
+//                readBall[readBall.length-i-1] = temp;
+            }
+
+        }
+        return userReadBall;
     }
 }
--- src/test/java/com/example/randomset/util/RandomSetUtilsWithSmallDataTest.java
+++ src/test/java/com/example/randomset/util/RandomSetUtilsWithSmallDataTest.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -14,30 +13,35 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @author FZH
  * @date 2022/08/30
  */
-class RandomSetUtilsTest {
-    private static final Logger LOGGER = getLogger(RandomSetUtilsTest.class);
+class RandomSetUtilsWithSmallDataTest {
+    private static final Logger LOGGER = getLogger(RandomSetUtilsWithSmallDataTest.class);
 
     /**
      * 使用Set集合实现生成不重复随机数
      */
     @Test
-    void random1() {
-        LOGGER.debug(RandomSetUtils.random1(33, 6).toString());
+    void randomSet1() {
+        LOGGER.debug(RandomSetUtils.randomSet1(33, 6).toString());
     }
 
     /**
      * 使用byte数组实现生成不重复随机数
      */
     @Test
-    void random2() {
-        LOGGER.debug(Arrays.toString(RandomSetUtils.random2(33, 6)));
+    void randomSet2() {
+        LOGGER.debug(Arrays.toString(RandomSetUtils.randomSet2(33, 6)));
     }
 
     /**
      * 使用类似开放寻址方式生成不重复随机数实现3
      */
     @Test
-    void random3() {
-        LOGGER.debug(Arrays.toString(RandomSetUtils.random3(33, 6)));
+    void randomSet3() {
+        LOGGER.debug(Arrays.toString(RandomSetUtils.randomSet3(33, 6)));
+    }
+
+    @Test
+    void randomSet4() {
+        LOGGER.debug(Arrays.toString(RandomSetUtils.randomSet4(33, 6)));
     }
 }
